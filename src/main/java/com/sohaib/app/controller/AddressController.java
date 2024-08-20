cb55309ac4a0efa394fbbc29f15d99d1892708e7
SohaibAli976 <59691179+SohaibAli976@users.noreply.github.com>
2024-08-20 17:57:47 +0500

    Config Server Changes
    Config Server Changes

--- src/main/java/com/sohaib/app/controller/AddressController.java
+++ src/main/java/com/sohaib/app/controller/AddressController.java
@@ -5,15 +5,20 @@ import com.sohaib.app.request.CreateAddressRequest;
 import com.sohaib.app.response.AddressResponse;
 import com.sohaib.app.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/address")
+@RefreshScope
 public class AddressController {
 
     @Autowired
     AddressService addressService;
 
+    @Value("${address.test}")
+    private String test;
 
     @PostMapping("/create")
     public AddressResponse createAddress(@RequestBody CreateAddressRequest address) {
@@ -23,5 +28,10 @@ public class AddressController {
     @GetMapping("/getById/{id}")
     public AddressResponse getAddressById(@PathVariable("id") Long id) {
         return addressService.getById(id);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return test;
     }
 }
